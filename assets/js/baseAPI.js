d41560c147d0963412fc0a1d8074ca43b1cad8fa
zsl <1043790504@qq.com>
2022-01-24 18:32:00 +0800

    完成主页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,26 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    options.url = 'http://www.liulongbin.top:3007' + options.url;
+    // 统一为有权限的接口设置headers的时候 有my的接口
+    if (options.url.indexOf('/my/' != -1)) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 权局统一调用complete
+    options.complete = function(res) {
+        console.log('ok');
+        console.log(res);
+        //在complete回调函数中可以使用responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+            //强制清空token 强制跳转登录页
+            console.log(0);
+            localStorage.removeItem('token');
+            location.href = '登录.html';
+        }
+
+    }
+
 })
---
+++ assets/js/主页.js
@@ -0,0 +1,76 @@
+window.onload = function() {
+    get()
+    let btn = document.getElementById("bth");
+    btn.onclick = function() {
+        // 提示客户是否退出
+
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            //  清空本地存储的token 重新跳转到登录页
+            localStorage.removeItem('token');
+            location.href = '登录.html';
+            // 关闭这个弹出框
+            layer.close(index);
+        });
+    }
+
+    function get() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            // 请求头
+
+            success: function(res) {
+                if (res.status != 0) {
+                    return layui.layer.msg('获取用户失败')
+                } else {
+                    //调用渲染头像的函数
+                    console.log(res.data);
+                    reader(res.data);
+                    // 在ajax以后都会执行complate函数
+
+                }
+            }
+
+        })
+    }
+
+    function reader(user) {
+        //获取用户的名称
+        let name = user.nickname || user.username;
+        console.log(name);
+        let huan = document.getElementById('huan')
+        huan.innerHTML = '欢迎&nbsp;&nbsp;' + name;
+        let tu = document.getElementsByClassName('layui-nav-img');
+        console.log(tu[0].src);
+        let wen = document.getElementsByClassName('tou');
+        console.log(wen);
+        // 按需渲染用户头像
+        if (user.user_pic != null) {
+            //图片头像
+            for (let i = 0; i < tu.length; i++) {
+                tu[i].src = user.user_pic;
+            }
+            for (let i = 0; i < wen.length; i++) {
+                wen[i].style.display = 'none';
+            }
+
+        } else {
+            //文本头像
+            let tu = document.getElementsByClassName('layui-nav-img');
+            let wen = document.getElementsByClassName('tou');
+            console.log(tu[0]);
+            for (let i = 0; i < tu.length; i++) {
+                tu[i].className = 'layui-nav-img guan';
+            }
+            let first = name[0].toUpperCase();
+            console.log(first);
+            for (let i = 0; i < wen.length; i++) {
+                wen[i].innerHTML = first;
+                wen[i].className = 'tou kai';
+            }
+
+
+        }
+    }
+}
